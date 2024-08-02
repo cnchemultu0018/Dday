b6d8b8dfe6718994e49f6bfc3a2beabc86fc9100
cnchemulti <cnchemulti@g.cnees.kr>
2024-08-02 11:59:24 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     // 디데이 날짜 설정 (YYYY-MM-DD 형식)
-    const ddayDate = new Date('2024-10-12');
+    const ddayDate = new Date('2024-10-11');
 
     function updateCountdown() {
+        const KR_TIME_DIFF = 1000*60*60*9;
         const currentDate = new Date();
-        const timeDifference = ddayDate.getTime() - currentDate.getTime();
+        const timeDifference = (ddayDate.getTime() + KR_TIME_DIFF) - (currentDate.getTime() + KR_TIME_DIFF);
 
         // 일, 시간, 분, 초 계산
-        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24) + 1);
         const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
